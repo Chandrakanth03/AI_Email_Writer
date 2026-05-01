fd54f9056f18f5bfa4e10a4875e39ae923dbcd81
Chandrakanth03 <22211a04b6@bvrit.ac.in>
2026-05-01 23:56:40 +0530

    Fix static file serving and add frontend controller

---
+++ src/main/java/com/emailwriter/FrontendController.java
@@ -0,0 +1,14 @@
+package com.emailwriter;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FrontendController {
+
+    @GetMapping("/")
+    public String index() {
+        return "forward:/index.html";
+    }
+}
+
